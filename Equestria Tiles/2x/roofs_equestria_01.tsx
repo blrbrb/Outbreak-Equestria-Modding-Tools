b8690a05d2aa03d3d3a49e9c5565c3636d52cc2d
Eli Lishman Reynolds <elireynolds23@gmail.com>
2023-06-22 19:42:36 -0400

    6/22/2023

--- Equestria Tiles/2x/roofs_equestria_01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="roofs_equestria_01" tilewidth="64" tileheight="32">
- <image source="roofs_equestria_01.png" width="1024" height="2048"/>
-</tileset>
